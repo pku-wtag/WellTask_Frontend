e30d6d5ccf64f5738b0541002b01aeeae9964bdb
pku-wtag <proshanto.kumar@welldev.io>
2025-09-21 14:04:01 +0600

    solve merge issues

--- WellTask_Frontend/src/App.tsx
+++ WellTask_Frontend/src/App.tsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import RoutesComponent from "@/RoutesComponent";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "@/components/login";
+import SignupPage from "@/components/sign-up";
+import ForgotPasswordPage from "@/components/ForgotPasswordPage";
+import VerifyCodePage from "@/components/VerifyCodePage";
+import ResetPasswordPage from "@/components/ResetPasswordPage";
+
+
 
 const App: React.FC = () => {
   return (
     <Router>
-      <RoutesComponent />
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/verify-code" element={<VerifyCodePage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+      </Routes>
     </Router>
   );
 };
