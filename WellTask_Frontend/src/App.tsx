9bd5dcbf2df32b26e46b90990907e29030c38db1
pku-wtag <proshanto.kumar@welldev.io>
2025-09-21 15:11:53 +0600

    remove duplicate file

--- WellTask_Frontend/src/App.tsx
+++ WellTask_Frontend/src/App.tsx
@@ -1,28 +1,10 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import LoginPage from "@/components/login";
-import SignupPage from "@/components/sign-up";
-import ForgotPasswordPage from "./components/forgot-password";
-import VerifyCodePage from "@/components/verify-code";
-import ResetPasswordPage from "@/components/reset-password";
-
-
+import { BrowserRouter as Router } from "react-router-dom";
+import RoutesComponent from "@/RoutesComponent";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/verify-code" element={<VerifyCodePage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-      </Routes>
+      <RoutesComponent />
     </Router>
   );
 };
